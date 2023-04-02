ac011e87595d571e957a806b6b4dc428962315e2
Kosmatoff <dimka.komarov@bk.ru>
2023-04-03 00:13:21 +0300

    Удаление случайно добавленного файла dz1

--- dz1/download.sh
+++
@@ -1,6 +0,0 @@
-wget https://datasets.imdbws.com/title.basics.tsv.gz
-wget https://datasets.imdbws.com/title.akas.tsv.gz
-wget https://datasets.imdbws.com/title.ratings.tsv.gz
-gunzip *.tsv.gz
-mkdir dataset
-mv *.tsv dataset
--- dz1/include/movie_const.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2023 Kosmatoff
-#pragma once
-#include <string>
-
-static const char splitter = '\t';
-static const std::string movie = "movie";
-static const int topMovies = 10;
